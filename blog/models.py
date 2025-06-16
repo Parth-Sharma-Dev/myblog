796e2727d55bcaf0ea3aa84465ad2b65f038e022
Parth Sharma <parthdausa5@gmail.com>
2025-06-16 20:06:53 +0530

    add Contact model

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,9 @@ class Post(models.Model):
     title = models.CharField(100)
     body = models.CharField(1000000)
     create_at = models.DateTimeField(default=datetime.now, blank=True)
+
+class Contact(models.Model):
+    name = models.CharField(100)
+    email = models.EmailField(100)
+    message = models.TextField()
+    date_time = models.DateTimeField()
