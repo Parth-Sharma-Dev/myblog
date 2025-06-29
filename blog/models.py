49c2913f502bcd26c150bc190c25ef0fcc0c4f88
Parth Sharma <parthdausa5@gmail.com>
2025-06-29 10:07:45 +0530

    completed contact form, connected it to database.

--- blog/models.py
+++ blog/models.py
@@ -3,12 +3,15 @@ from datetime import datetime
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(100)
-    body = models.CharField(1000000)
+    title = models.CharField(max_length=100)
+    body = models.CharField(max_length=1000000)
     create_at = models.DateTimeField(default=datetime.now, blank=True)
 
 class Contact(models.Model):
-    name = models.CharField(100)
-    email = models.EmailField(100)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
     message = models.TextField()
     date_time = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Contact
+from datetime import datetime
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -14,4 +16,12 @@ def about(request):
     return render(request, 'about.html')
 
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        message = request.POST['message']
+        contact = Contact(name=name, email=email, message=message, date_time=datetime.today())
+        contact.save()
+        messages.success(request, ' Your message has been sent.')
+
     return render(request, 'contact.html')
